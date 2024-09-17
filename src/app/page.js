4d809c0ad6d8f39962a4936bcdf5affb77ca5dc7
rudietuesdays <rae@throneless.tech>
2024-09-16 21:29:29 -0400

    initial commit with landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,70 @@
+// NextJS imports
 import Image from "next/image";
-import styles from "./page.module.css";
+
+// ChakraUI imports
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  LinkBox,
+  LinkOverlay,
+  Text,
+  textDecoration,
+  VisuallyHidden
+} from "@chakra-ui/react";
+
+// icons
+import { FaInstagram } from "react-icons/fa";
+
+//styles
+// import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box bg='purple.700' color='white' height='100vh' width='100vw'>
+      <Container
+        paddingY={20}
+        maxW='container.md'
+        textAlign='center'
+      >
+        <Heading as='h1'>
+          Queer Elders Podcast
+        </Heading>
+        <Text fontSize={18} marginTop={12}>
+          A podcast for anyone who wants to imagine new, queerer futures by learning from queer and trans elders today – because we have always been here, and we always will be.
+        </Text>
+        <Text fontSize={18} marginTop={6}>
+          More coming soon. Stay tuned.
+        </Text>
+        <LinkBox
+          sx={{
+            '_hover': {
+              textDecoration: 'underline',
+            }
+          }}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <HStack
+          alignItems='center'
+          justifyContent='center'
+          marginY={12}
+          textAlign='center'
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <FaInstagram />
+            <LinkOverlay href='https://www.instagram.com/queer.elders.pod/'>
+            <Text as='span'>
+              queer.elders.pod
+            </Text>
+            <VisuallyHidden>
+              <Text as='span'>
+                {' '}on Instagram
+              </Text>
+            </VisuallyHidden>
+            </LinkOverlay>
+        </HStack>
+      </LinkBox>
+      </Container>
+    </Box>
   );
 }
